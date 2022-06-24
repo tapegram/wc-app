732ffacb72657e6640169531f3d3f514520eabe3
orcunTasdemir <otasdemir@vassar.edu>
2022-06-24 03:05:53 -0400

    tailwind css navigation base version

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
 import { MetaTags } from '@redwoodjs/web'
 import WallChartCell from 'src/components/WallchartCell'
-
+import Navbar from 'src/components/Navbar'
 const HomePage = () => (
-    <>
-      <WallChartCell id={1} />
-      <MetaTags title="Home" description="Home page" />
-    </>
-  )
+  <>
+    <WallChartCell id={1} />
+    <MetaTags title="Home" description="Home page" />
+  </>
+)
 
 export default HomePage
