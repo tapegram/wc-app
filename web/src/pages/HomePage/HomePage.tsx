8f2f2ea9379854f692565b669c63982f61ec8359
Ian <bontaq@gmail.com>
2022-07-13 18:21:54 -0400

    sp

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import { MetaTags } from '@redwoodjs/web'
-import WallChartCell from 'src/components/WallchartCell'
+import WallChartCell from 'src/components/WallChartCell'
 import Navbar from 'src/components/Navbar'
 const HomePage = () => (
   <>
